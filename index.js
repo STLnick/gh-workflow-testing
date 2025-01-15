4dd584f21256e61ac7fc25e063fdf921bcdce98f
Nick Ray <nickrray@gmail.com>
2025-01-15 08:54:04 -0600

    Add log

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
+console.log('STAGE');
+
 const server = http.createServer((_, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello, world!\n');
